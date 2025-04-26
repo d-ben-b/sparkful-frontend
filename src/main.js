a0d5a17edd5fbf114d5eb4668d5525ae3ebf960d
d-ben-b <155895903+d-ben-b@users.noreply.github.com>
2025-04-26 09:01:04 +0800

    feat: update axios base URL to new ngrok endpoint for API access

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ const pinia = createPinia() // Create pinia instance first
 const app = createApp(App)
 
 // axios.defaults.baseURL = 'https://4efd-140-116-49-123.ngrok-free.app/api/'
-axios.defaults.baseURL = 'https://4c8f-2001-b011-3012-12f2-9dd0-ed6f-7721-ae28.ngrok-free.app/api/'
+axios.defaults.baseURL = 'https://8ecf-2402-7500-578-27f0-7dba-8259-e828-c3ee.ngrok-free.app/api/'
 axios.defaults.headers.common['ngrok-skip-browser-warning'] = 'true'
 app.config.globalProperties.$axios = axios
 
